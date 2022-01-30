ec3eee4d82c21705e62ca299f328a976c8524dc4
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-01-30 22:45:05 +0530

    Create RenameFileJavaDemo.java

---
+++ RenameFileJavaDemo.java
@@ -0,0 +1,21 @@
+import java.io.File;
+public class RenameFileJavaDemo
+{
+    public static void main(String[] args)
+    {	
+        //Old File
+	File oldfile =new File("C:\\myfile.txt");
+	//New File
+	File newfile =new File("C:\\mynewfile.txt");
+	/*renameTo() return boolean value
+	 * It return true if rename operation is
+	 * successful
+	 */
+        boolean flag = oldfile.renameTo(newfile);
+	if(flag){
+	   System.out.println("File renamed successfully");
+	}else{
+	   System.out.println("Rename operation failed");
+	 }
+    }
+}

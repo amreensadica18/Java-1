3dd3f3a40e2616d2d014aa01dbbb920726b72d8e
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-02-18 00:00:51 +0530

    Create Convert_DoubleToStringExample.java

---
+++ Convert_DoubleToStringExample.java
@@ -0,0 +1,12 @@
+public class DoubleToStringExample{  
+   public static void main(String args[]){ 
+	//double value
+	double dnum = 99.9999;  
+		
+	//convert double to string using valueOf() method
+	String str = String.valueOf(dnum);  
+		
+	//displaying output string after conversion
+	System.out.println("My String is: "+str);  
+   }
+}

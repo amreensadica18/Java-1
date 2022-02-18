fa2e90f725541b5e734c66993f64c8f898e54b5d
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-02-18 23:33:01 +0530

    Create Convert_intToDoubleExample.java

---
+++ Convert_intToDoubleExample.java
@@ -0,0 +1,12 @@
+public class IntToDoubleExample{  
+   public static void main(String args[]){ 
+	//int value
+	int inum = 101;
+		
+	//int to double conversion without typecasting
+	double dnum = inum;  
+		
+	//displaying double value
+	System.out.println(dnum);  
+   }
+}

ce836b701422221b7cf9da25c0aa082c6a62c4e5
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-02-21 22:02:11 +0530

    Create Convert_LongToIntExample.java

---
+++ Convert_LongToIntExample.java
@@ -0,0 +1,9 @@
+public class LongToIntExample{  
+   public static void main(String args[]){  
+	long lnum = 1000;  
+		
+	//type casting - long to int
+	int inum = (int)lnum;  
+	System.out.println("Converted int value is: "+inum);  
+   }
+}

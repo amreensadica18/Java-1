b7bd4302b25705ac5d0d7ea75aca8fc6717dee78
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-02-22 19:37:18 +0530

    Create Convert_CharToStringExample.java

---
+++ Convert_CharToStringExample.java
@@ -0,0 +1,12 @@
+public class CharToStringExample{  
+    public static void main(String args[]){  
+	//given char
+	char ch = 'P';
+		
+	//char to string conversion
+	String str = String.valueOf(ch);  
+		
+	//displaying the string
+	System.out.println("String after conversion is: "+str);    
+   }
+}

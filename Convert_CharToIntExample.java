a0c9e6ea76729814583013d82d6387d1bcabaf15
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-02-23 21:32:17 +0530

    Create Convert_CharToIntExample.java

---
+++ Convert_CharToIntExample.java
@@ -0,0 +1,10 @@
+public class CharToIntExample{  
+   public static void main(String args[]){  
+	char ch = 'A';
+	char ch2 = 'Z';
+	int num = ch;
+	int num2 = ch2;
+	System.out.println("ASCII value of char "+ch+ " is: "+num);
+	System.out.println("ASCII value of char "+ch2+ " is: "+num2);
+   }
+}

515382fd352a90ba712fe79ff39ecd86409b889e
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-02-24 19:54:59 +0530

    Create Convert_IntToCharExample.java

---
+++ Convert_IntToCharExample.java
@@ -0,0 +1,9 @@
+public class IntToCharExample{  
+   public static void main(String args[]){  
+	//integer number
+	int num = 70;
+	//type casting
+	char ch = (char)num;  
+	System.out.println(ch);  
+   }
+}

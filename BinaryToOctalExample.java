cae41d39956ba348420941124c727ba6fc2cdc64
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-03-01 21:37:56 +0530

    Create BinaryToOctalExample.java

---
+++ BinaryToOctalExample.java
@@ -0,0 +1,8 @@
+public class BinaryToOctalExample{  
+   public static void main(String args[]){  
+	String number = "10101";
+	int bnum = Integer.parseInt(number, 2);
+	String ostr = Integer.toOctalString(bnum);
+	System.out.println("Octal Value after conversion is: "+ostr);
+   }
+}

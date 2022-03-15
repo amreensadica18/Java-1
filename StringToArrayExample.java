cc2ccb61519c8f728e3b7b36e04740e87edc97d7
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-03-15 22:04:46 +0530

    Create StringToArrayExample.java

---
+++ StringToArrayExample.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+public class StringToArrayExample {
+    public static void main(String args[]){
+	String num = "22,33,44,55,66,77";
+	String str[] = num.split(",");
+	List<String> al = new ArrayList<String>();
+	al = Arrays.asList(str);
+	for(String s: al){
+	   System.out.println(s);
+	}
+   }
+}

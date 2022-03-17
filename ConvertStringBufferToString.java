9326c397d6868ebd26afba526cd40c70ba279fc7
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-03-17 21:05:52 +0530

    Create ConvertStringBufferToString.java

---
+++ ConvertStringBufferToString.java
@@ -0,0 +1,8 @@
+public class ConvertStringBufferToString {
+   public static void main(String[] args) {
+		  
+      StringBuffer sb = new StringBuffer("This is prabha");
+      String str = sb.toString();
+      System.out.println("String object: "+str);
+   }
+}

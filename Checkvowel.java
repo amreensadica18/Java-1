452c4c609a5c53c7b6e212c5ed28055715df121a
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-02-14 20:06:45 +0530

    Create Checkvowel.java

---
+++ Checkvowel.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Checkvowel
+{
+   public static void main(String[ ] arg)
+   {
+	boolean isVowel=false;;
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Enter a character : ");
+	char ch=scanner.next().charAt(0); 
+	scanner.close();
+	switch(ch)
+	{
+	   case 'a' :
+	   case 'e' :
+       case 'i' :
+	   case 'o' :
+	   case 'u' :
+	   case 'A' :
+	   case 'E' :
+	   case 'I' :
+	   case 'O' :
+	   case 'U' :
+	       System.out.println(ch+" is a vowel");
+	       break;
+	   default :
+	       System.out.println(ch+ " Is a consonant");
+	}
+   }
+}

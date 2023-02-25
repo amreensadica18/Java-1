e8ae6422c862884f1d43b759b958e8c438995d0f
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-02-25 19:44:42 +0530

    Create ForeachLoop.java

---
+++ ForeachLoop.java
@@ -0,0 +1,10 @@
+//ForeachLoop in arrays.
+public class ForeachLoop {
+    public static void main(String[]args){
+        int []a={1,2,3,4,5};
+        
+       for(int x:a){ //each value is stored one by one in "x".
+             System.out.println(x);
+             }
+    }
+}

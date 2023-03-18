7776c0ba01b3531c70f2fd765722e232e31d20ea
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-03-18 20:26:44 +0530

    Create ArrayBasics18.java

---
+++ ArrayBasics18.java
@@ -0,0 +1,10 @@
+public class ArrayBasics18
+{  
+public static void main(String args[])
+{  
+int arr[]={33,3,4,5};  
+//printing array using for-each loop  
+ for(int i:arr)  
+   System.out.println(i);  
+ }
+}  

e95ce1749bf4dbfbb5a17f1c931740f554d4a6bd
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-03-30 20:52:22 +0530

    Create ArrayBasics30.java

---
+++ ArrayBasics30.java
@@ -0,0 +1,13 @@
+public class ArrayBasics30 {
+ public static void main(String[] args) {
+   // create an array
+   int[] age = {1, 4, 5};
+
+   // loop through the array
+   // using for loop
+   System.out.println("Using for Loop:");
+   for(int i = 0; i < age.length; i++) {
+     System.out.println(age[i]);
+   }
+ }
+}

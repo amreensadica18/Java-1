0b833e745ec07f5be02ba582d310e0b24e2ab2ce
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-04-03 22:06:50 +0530

    Create StringExample4.java

---
+++ StringExample4.java
@@ -0,0 +1,7 @@
+public class StringExample4{  
+ public static void main(String args[]){  
+   String s="Java";  
+   s.concat(" Programming");//concat() method appends the string at the end  
+   System.out.println(s);//will print Sachin because strings are immutable objects  
+ }  
+}  

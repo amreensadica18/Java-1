f913b90728128f30dea79a5fd9d893958cb9e112
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-04-07 21:21:51 +0530

    Create StringExample8.java

---
+++ StringExample8.java
@@ -0,0 +1,11 @@
+public class StringExample8{  
+public static void main(String args[]){  
+String name="Prabha";  
+String s1=String.format("name is %s",name);  
+String s2=String.format("value is %f",32.33434);  
+String s3=String.format("value is %32.12f",32.33434);  
+  
+System.out.println(s1);  
+System.out.println(s2);  
+System.out.println(s3);  
+}}  

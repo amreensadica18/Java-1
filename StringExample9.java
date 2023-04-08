ab273de8da23709b8617e42750e0b8dfe5156df4
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-04-08 23:18:43 +0530

    Create StringExample9.java

---
+++ StringExample9.java
@@ -0,0 +1,11 @@
+public class StringExample9{  
+
+public static void main(String args[]){  
+
+String s1="hello world";  
+
+String s1upper=s1.toUpperCase();  
+
+System.out.println(s1upper);  
+
+}}  

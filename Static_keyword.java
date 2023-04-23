6098e9b346bcaf2de02c48206ba9012314307791
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-04-23 22:12:25 +0530

    Create Static_keyword.java

---
+++ Static_keyword.java
@@ -0,0 +1,11 @@
+package com.aiite.sam2;
+public class Test4 {
+static String name = "Raj";
+public static void sample() {
+System.out.println("Sample Executing");
+}
+public static void main(String[] args) {
+System.out.println(name);
+sample();
+}
+}

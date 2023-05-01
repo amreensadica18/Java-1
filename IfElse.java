c66c107cfe0f56840efa3163f60c8b7a16ef0517
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-05-01 22:20:16 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,22 @@
+package com.aiite.sam2;
+
+public class Test3 {
+
+public static void main(String[] args) {
+
+int age = 17;
+  if (age>=18) {
+
+System.out.println("You are Eligible to Vote");
+
+} else {
+
+System.out.println("You are not eligible");
+
+}
+
+}
+
+}
+
+C

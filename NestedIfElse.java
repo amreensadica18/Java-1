5f08ecf5759c2d1185651551d26c25634f87d127
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-05-02 22:43:53 +0530

    Create NestedIfElse.java

---
+++ NestedIfElse.java
@@ -0,0 +1,25 @@
+package com.aiite.sam2;
+
+public class NestedIfElse {
+
+public static void main(String[] args) {
+
+int age = 17;
+
+if (age>=18 && age<=59) {
+
+System.out.println("Major");
+
+} else if (age>=60) {
+
+System.out.println("Senior");
+
+} else if (age<18) {
+
+System.out.println("Minor");
+
+}
+
+}
+
+}

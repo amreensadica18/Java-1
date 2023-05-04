99e533ace1e99b54613fa6a55041f120195b1fe1
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-05-04 21:46:45 +0530

    Create SwitchStatement.java

---
+++ SwitchStatement.java
@@ -0,0 +1,20 @@
+package com.sample;
+public class SwitchStatement {
+public static void main(String[] args) {
+int percent = 90;
+switch (percent) {
+case 90:
+System.out.println("Grade A");
+break;
+case 80:
+System.out.println("Grade B");
+break;
+case 70:
+System.out.println("Grade C");
+break;
+default:
+System.out.println("not mentioned in above Case");
+break;
+}
+}
+}

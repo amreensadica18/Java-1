f6dba2f72904859cf751aa1f2b8d5bcee6f89e0e
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-05-23 22:34:05 +0530

    Create CharToString.java

---
+++ CharToString.java
@@ -0,0 +1,7 @@
+public class CharString {
+  public static void main(String[] args) {
+        char ch = 'a';
+        String st = Character.toString(ch);
+        System.out.println("The string is: " + st);
+    }
+}

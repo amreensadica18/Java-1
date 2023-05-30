1c81f90328c1238ebd381ad0bc98d70eb0325de2
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-05-30 22:28:13 +0530

    Create StringToInt.java

---
+++ StringToInt.java
@@ -0,0 +1,12 @@
+public class StringToInt {
+  public static void main(String[] args) {
+
+    String str1 = "25";
+    String str2 = "66";
+    int num1 = Integer.parseInt(str1);
+    int num2 = Integer.parseInt(str2);
+
+    System.out.println(num1);    
+    System.out.println(num2);   
+  }
+}

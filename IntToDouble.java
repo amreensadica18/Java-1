2c1596b759e4b6fe39bb1a398a67ba28552d4871
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-06-01 21:44:21 +0530

    Create IntToDouble.java

---
+++ IntToDouble.java
@@ -0,0 +1,12 @@
+public class IntToDouble {
+  public static void main(String[] args) {
+
+    int a =30;
+    int b = 20;
+    double c = a;
+    double d = b;
+
+    System.out.println(c);   
+    System.out.println(d);   
+  }
+}

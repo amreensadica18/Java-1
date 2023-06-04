45b14dd3668c22b8145dacc721d81821c6b2e90c
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-06-04 22:52:47 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,15 @@
+public class LeapYear {
+  public static void main(String[] args) {
+    int year = 1800;
+    boolean leap = false;
+    if (year % 4 == 0) {
+
+      if (year % 100 == 0) {
+        if (year % 400 == 0)
+          leap = true;
+        else
+          leap = false;
+      }
+      else
+        leap = true;
+    }

fe27ab277c11ab3664270eac169df2ed45efa01f
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-06-20 22:41:03 +0530

    Create TimeString.java

---
+++ TimeString.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TimeString {
+
+    public static void main(String[] args) {
+        String string = "2020-08-15";
+        LocalDate date = LocalDate.parse(string, DateTimeFormatter.ISO_DATE);
+
+        System.out.println(date);
+    }
+}
